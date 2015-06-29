174cfce9698930a8d07fd573b38da55d6c18ca09
Keenan Lang <klang@aps.anl.gov>
2015-06-29 15:25:24 -0500

    Added a demonstration of asyn support library

---
+++ iocBoot/iocLua/asyn.cmd
@@ -0,0 +1,21 @@
+# Linux startup script
+
+< ./nfsCommands
+
+#load("../../bin/vxWorks-ppc32/scriptioc.munch")
+
+################################################################################
+# Tell EPICS all about the record types, device-support modules, drivers,
+# etc. in the software we just loaded (xxx.munch)
+dbLoadDatabase("../../dbd/scriptioc.dbd")
+scriptioc_registerRecordDeviceDriver(pdbbase)
+
+epicsEnvSet("LUA_SCRIPT_PATH",".")
+
+drvAsynIPPortConfigure("IP", "cars.uchicago.edu:80", 0,0,0)
+
+dbLoadRecords("./asyn.db", "PORT=IP")
+
+#######
+iocInit
+#######
---
+++ iocBoot/iocLua/asyn.lua
@@ -0,0 +1,15 @@
+WriteTimeout = 4.0
+OutTerminator = "\n\n"
+InTerminator = "\n"
+
+function get_html(port)
+	asyn.write("GET / HTTP/1.0", port)
+	local input = asyn.read(port)
+	
+	while (input ~= nil) do
+		print(input)
+	
+		input = asyn.read(port)
+	end
+	
+end
