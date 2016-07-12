a3adb107b32be4d1a51e34fd2409a7994e103e53
Keenan Lang <klang@aps.anl.gov>
2016-07-12 13:56:24 -0500

    Added writeread function to make device communication easier. Removed flush command because it wasn't currently working and writeread made it less useful.

--- scriptApp/src/lua/lasynlib.cpp
+++ scriptApp/src/lua/lasynlib.cpp
@@ -28,21 +28,20 @@ static int l_read(lua_State* state)
 	if (port == NULL) { return 0; }
 	
 	int isnum;
-	
-	lua_getglobal(state, "ReadTimeout");
-	double timeout = lua_tonumberx(state, -1, &isnum);
-	lua_remove(state, -1);
-	
-	lua_getglobal(state, "InTerminator");
-	const char* in_term = lua_tostring(state, -1);
-	lua_remove(state, -1);
-	
 	std::string output;
 	
 	try
 	{
 		asynOctetClient input(port, addr, param);
 		
+        lua_getglobal(state, "ReadTimeout");
+        double timeout = lua_tonumberx(state, -1, &isnum);
+        lua_remove(state, -1);
+        
+        lua_getglobal(state, "InTerminator");
+        const char* in_term = lua_tostring(state, -1);
+        lua_remove(state, -1);
+        
 		if (isnum)    { input.setTimeout(timeout); }
 		if (in_term)  { input.setInputEos(in_term, strlen(in_term)); }
 		
@@ -91,25 +90,25 @@ static int l_write(lua_State* state)
 	
 	int isnum;
 	
-	lua_getglobal(state, "WriteTimeout");
-	double timeout = lua_tonumberx(state, -1, &isnum);
-	lua_remove(state, -1);
-	
-	lua_getglobal(state, "OutTerminator");
-	const char* out_term = lua_tostring(state, -1);
-	lua_remove(state, -1);
-	
 	try
 	{
 		asynOctetClient output(port, addr, param);
 			
+        lua_getglobal(state, "WriteTimeout");
+        double timeout = lua_tonumberx(state, -1, &isnum);
+        lua_remove(state, -1);
+        
+        lua_getglobal(state, "OutTerminator");
+        const char* out_term = lua_tostring(state, -1);
+        lua_remove(state, -1);
+        
 		if (isnum)       { output.setTimeout(timeout); }
 		if (out_term)    { output.setOutputEos(out_term, strlen(out_term)); }
 			
-		size_t numwrite;
-        
+		size_t numwrite;       
 		output.write(data, strlen(data), &numwrite);
-		return 0;
+        
+        return 0;
 	}
 	catch (...)
 	{
@@ -119,33 +118,81 @@ static int l_write(lua_State* state)
 	return 0;
 }
 
-static int l_flush(lua_State* state)
+static int l_writeread(lua_State* state)
 {
     int num_ops = lua_gettop(state);
 	
-	if (num_ops < 1 || num_ops > 3)    { return 0; }
+	if (num_ops < 2 || num_ops > 4)    { return 0; }
 	
 	if (num_ops >= 1 && ! lua_isstring(state, 1))    { return 0; }
-	if (num_ops >= 2 && ! lua_isnumber(state, 2))    { return 0; }
-	if (num_ops == 3 && ! lua_isstring(state, 3))    { return 0; }
- 
-    const char* port = lua_tostring(state, 1);
-	int addr = (int) lua_tonumber(state, 2);
-	const char* param = lua_tostring(state, 3);
+	if (num_ops >= 2 && ! lua_isstring(state, 2))    { return 0; }
+	if (num_ops >= 3 && ! lua_isnumber(state, 3))    { return 0; }
+	if (num_ops == 4 && ! lua_isstring(state, 4))    { return 0; }
 	
-	if (port == NULL) { return 0; }
+	
+	const char* data = lua_tostring(state, 1);
+	const char* port = lua_tostring(state, 2);
+	int addr = (int) lua_tonumber(state, 3);
+	const char* param = lua_tostring(state, 4);
+	
+	if (port == NULL)    { return 0; }
+	
+	int isnum;	
+    std::string output;
     
-    try
+	try
 	{
-		asynOctetClient input(port, addr, param);
-        input.flush();
-    }
-    catch (...)
-    {
-      return 0;
-    }
-    
-    return 0;
+		asynOctetClient client(port, addr, param);
+
+        double timeout;
+        
+        lua_getglobal(state, "WriteTimeout");
+        timeout = lua_tonumberx(state, -1, &isnum);
+        lua_remove(state, -1);
+        
+        lua_getglobal(state, "OutTerminator");
+        const char* out_term = lua_tostring(state, -1);
+        lua_remove(state, -1);
+        
+		if (isnum)       { client.setTimeout(timeout); }
+		if (out_term)    { client.setOutputEos(out_term, strlen(out_term)); }
+			
+		size_t numwrite;       
+		client.write(data, strlen(data), &numwrite);
+        
+        lua_getglobal(state, "ReadTimeout");
+        timeout = lua_tonumberx(state, -1, &isnum);
+        lua_remove(state, -1);
+        
+        lua_getglobal(state, "InTerminator");
+        const char* in_term = lua_tostring(state, -1);
+        lua_remove(state, -1);
+        
+        if (isnum)    { client.setTimeout(timeout); }
+		if (in_term)  { client.setInputEos(in_term, strlen(in_term)); }
+		
+		char buffer[128];
+				
+		size_t numread;
+		int eomReason;
+		
+		do
+		{
+			client.read(buffer, sizeof(buffer), &numread, &eomReason);
+			output += std::string(buffer, numread);
+		} while (eomReason & ASYN_EOM_CNT);
+		
+		if (output.empty())    { return 0; }
+        
+		lua_pushstring(state, output.c_str());
+		return 1;      
+	}
+	catch (...)
+	{
+		return 0;
+	}
+	
+	return 0;
 }
 
 static int l_setOutTerminator(lua_State* state)
@@ -315,29 +362,31 @@ static int l_getDoubleParam(lua_State* state)
 
 static int l_callParamCallbacks(lua_State* state)
 {
-  int num_ops = lua_gettop(state);
-  
-  if (num_ops < 1 || num_ops > 2)    { return 0; }
-  
-  if (! lua_isstring(state, 1))      { return 0; }
-  
-  if (num_ops == 2 && ! lua_isnumber(state, 2))    { return 0; }
-  
-  const char* port = lua_tostring(state, 1);
-  
-  int addr = 0;
-  
-  if (num_ops == 2)    { addr = (int) lua_tonumber(state, 2); }
-  
-  asynPortDriver* driver = (asynPortDriver*) findAsynPortDriver(port);
-  
-  driver->callParamCallbacks(addr);
+    int num_ops = lua_gettop(state);
+    
+    if (num_ops < 1 || num_ops > 2)    { return 0; }
+    
+    if (! lua_isstring(state, 1))      { return 0; }
+    
+    if (num_ops == 2 && ! lua_isnumber(state, 2))    { return 0; }
+    
+    const char* port = lua_tostring(state, 1);
+    
+    int addr = 0;
+    
+    if (num_ops == 2)    { addr = (int) lua_tonumber(state, 2); }
+    
+    asynPortDriver* driver = (asynPortDriver*) findAsynPortDriver(port);
+    
+    driver->callParamCallbacks(addr);
+    
+    return 0;
 }
 
 static const luaL_Reg mylib[] = {
 	{"read", l_read},
 	{"write", l_write},
-    {"flush", l_flush},
+    {"writeread", l_writeread},
 	{"setOutTerminator", l_setOutTerminator},
 	{"setInTerminator", l_setInTerminator},
 	{"setWriteTimeout", l_setWriteTimeout},
