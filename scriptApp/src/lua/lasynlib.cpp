4692011b07fed3f09d11a6e0236ebd2003602da1
Keenan Lang <klang@aps.anl.gov>
2016-09-15 10:36:44 -0500

    Whitespace formatting

--- scriptApp/src/lua/lasynlib.cpp
+++ scriptApp/src/lua/lasynlib.cpp
@@ -34,14 +34,14 @@ static int l_read(lua_State* state)
 	{
 		asynOctetClient input(port, addr, param);
 		
-        lua_getglobal(state, "ReadTimeout");
-        double timeout = lua_tonumberx(state, -1, &isnum);
-        lua_remove(state, -1);
-        
-        lua_getglobal(state, "InTerminator");
-        const char* in_term = lua_tostring(state, -1);
-        lua_remove(state, -1);
-        
+		lua_getglobal(state, "ReadTimeout");
+		double timeout = lua_tonumberx(state, -1, &isnum);
+		lua_remove(state, -1);
+		
+		lua_getglobal(state, "InTerminator");
+		const char* in_term = lua_tostring(state, -1);
+		lua_remove(state, -1);
+
 		if (isnum)    { input.setTimeout(timeout); }
 		if (in_term)  { input.setInputEos(in_term, strlen(in_term)); }
 		
@@ -57,7 +57,7 @@ static int l_read(lua_State* state)
 		} while (eomReason & ASYN_EOM_CNT);
 		
 		if (output.empty())    { return 0; }
-        
+
 		lua_pushstring(state, output.c_str());
 		return 1;
 	}
@@ -94,21 +94,21 @@ static int l_write(lua_State* state)
 	{
 		asynOctetClient output(port, addr, param);
 			
-        lua_getglobal(state, "WriteTimeout");
-        double timeout = lua_tonumberx(state, -1, &isnum);
-        lua_remove(state, -1);
-        
-        lua_getglobal(state, "OutTerminator");
-        const char* out_term = lua_tostring(state, -1);
-        lua_remove(state, -1);
-        
+		lua_getglobal(state, "WriteTimeout");
+		double timeout = lua_tonumberx(state, -1, &isnum);
+		lua_remove(state, -1);
+		
+		lua_getglobal(state, "OutTerminator");
+		const char* out_term = lua_tostring(state, -1);
+		lua_remove(state, -1);
+
 		if (isnum)       { output.setTimeout(timeout); }
 		if (out_term)    { output.setOutputEos(out_term, strlen(out_term)); }
 			
 		size_t numwrite;       
 		output.write(data, strlen(data), &numwrite);
-        
-        return 0;
+
+		return 0;
 	}
 	catch (...)
 	{
@@ -120,7 +120,7 @@ static int l_write(lua_State* state)
 
 static int l_writeread(lua_State* state)
 {
-    int num_ops = lua_gettop(state);
+	int num_ops = lua_gettop(state);
 	
 	if (num_ops < 2 || num_ops > 4)    { return 0; }
 	
@@ -137,37 +137,37 @@ static int l_writeread(lua_State* state)
 	if (port == NULL)    { return 0; }
 	
 	int isnum;
-    
+
 	try
 	{
 		asynOctetClient client(port, addr, param);
-        
-        lua_getglobal(state, "WriteReadTimeout");
-        double timeout = lua_tonumberx(state, -1, &isnum);
-        lua_remove(state, -1);
-        
-        lua_getglobal(state, "OutTerminator");
-        const char* out_term = lua_tostring(state, -1);
-        lua_remove(state, -1);
-        
-        lua_getglobal(state, "InTerminator");
-        const char* in_term = lua_tostring(state, -1);
-        lua_remove(state, -1);
-        
+
+		lua_getglobal(state, "WriteReadTimeout");
+		double timeout = lua_tonumberx(state, -1, &isnum);
+		lua_remove(state, -1);
+		
+		lua_getglobal(state, "OutTerminator");
+		const char* out_term = lua_tostring(state, -1);
+		lua_remove(state, -1);
+		
+		lua_getglobal(state, "InTerminator");
+		const char* in_term = lua_tostring(state, -1);
+		lua_remove(state, -1);
+
 		if (isnum)       { client.setTimeout(timeout); }
 		if (out_term)    { client.setOutputEos(out_term, strlen(out_term)); }
 		if (in_term)  { client.setInputEos(in_term, strlen(in_term)); }
-        
+
 		size_t numwrite, numread;
 		char buffer[256];
 		int eomReason;
 		
-        client.writeRead(data, strlen(data), buffer, sizeof(buffer), &numwrite, &numread, &eomReason);
-        
-        std::string output(buffer, numread);
+		client.writeRead(data, strlen(data), buffer, sizeof(buffer), &numwrite, &numread, &eomReason);
+		
+		std::string output(buffer, numread);
 		
 		if (output.empty())    { return 0; }
-        
+
 		lua_pushstring(state, output.c_str());
 		return 1;      
 	}
@@ -271,7 +271,7 @@ static int l_getIntegerParam(lua_State* state)
 	driver->findParam(addr, param, &index);
 	driver->getIntegerParam(addr, index, &value);
 	
-    lua_pushnumber(state, value);
+	lua_pushnumber(state, value);
 	return 1;
 }
 
@@ -339,8 +339,8 @@ static int l_getDoubleParam(lua_State* state)
 	
 	driver->findParam(addr, param, &index);
 	driver->getDoubleParam(addr, index, &value);
-    
-    lua_pushnumber(state, value);
+
+	lua_pushnumber(state, value);
 	return 1;
 }
 
@@ -406,40 +406,40 @@ static int l_getStringParam(lua_State* state)
 	
 	driver->findParam(addr, param, &index);
 	driver->getStringParam(addr, index, 255, value);
-    
+
 	std::string temp(value);
 	
-    lua_pushstring(state, temp.c_str());
+	lua_pushstring(state, temp.c_str());
 	return 1;
 }
 
 static int l_callParamCallbacks(lua_State* state)
 {
-    int num_ops = lua_gettop(state);
-    
-    if (num_ops < 1 || num_ops > 2)    { return 0; }
-    
-    if (! lua_isstring(state, 1))      { return 0; }
-    
-    if (num_ops == 2 && ! lua_isnumber(state, 2))    { return 0; }
-    
-    const char* port = lua_tostring(state, 1);
-    
-    int addr = 0;
-    
-    if (num_ops == 2)    { addr = (int) lua_tonumber(state, 2); }
-    
-    asynPortDriver* driver = (asynPortDriver*) findAsynPortDriver(port);
-    
-    driver->callParamCallbacks(addr);
-    
-    return 0;
+	int num_ops = lua_gettop(state);
+	
+	if (num_ops < 1 || num_ops > 2)    { return 0; }
+	
+	if (! lua_isstring(state, 1))      { return 0; }
+	
+	if (num_ops == 2 && ! lua_isnumber(state, 2))    { return 0; }
+	
+	const char* port = lua_tostring(state, 1);
+	
+	int addr = 0;
+	
+	if (num_ops == 2)    { addr = (int) lua_tonumber(state, 2); }
+	
+	asynPortDriver* driver = (asynPortDriver*) findAsynPortDriver(port);
+	
+	driver->callParamCallbacks(addr);
+	
+	return 0;
 }
 
 static const luaL_Reg mylib[] = {
 	{"read", l_read},
 	{"write", l_write},
-    {"writeread", l_writeread},
+	{"writeread", l_writeread},
 	{"setOutTerminator", l_setOutTerminator},
 	{"setInTerminator", l_setInTerminator},
 	{"setWriteTimeout", l_setWriteTimeout},
@@ -450,7 +450,7 @@ static const luaL_Reg mylib[] = {
 	{"getIntegerParam", l_getIntegerParam},
 	{"getDoubleParam", l_getDoubleParam},
 	{"getStringParam", l_getStringParam},
-    {"callParamCallbacks", l_callParamCallbacks},
+	{"callParamCallbacks", l_callParamCallbacks},
 	{NULL, NULL}  /* sentinel */
 };
 
--- scriptApp/src/lua/lepicslib.cpp
+++ scriptApp/src/lua/lepicslib.cpp
@@ -20,8 +20,8 @@ static int l_caget(lua_State* state)
 	chid id;
 
 	int status = ca_create_channel(pv_name, NULL, NULL, 0, &id);
-    
-    if (status != ECA_NORMAL) { return 0; }
+
+	if (status != ECA_NORMAL) { return 0; }
 	ca_pend_io(0.001);
 
 	switch (ca_field_type(id))
@@ -31,7 +31,7 @@ static int l_caget(lua_State* state)
 			struct dbr_time_string val;
 
 			ca_get(DBR_TIME_STRING, id, &val);
-            
+  
 			lua_pushstring(state, val.value);
 			break;
 		}
