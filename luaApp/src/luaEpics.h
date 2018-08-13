0511a0cdb82a97c7ace2e413e7683d4f72c8102e
klang <klang@aps.anl.gov>
2018-08-13 13:24:37 -0500

    Added static library registration function

--- luaApp/src/luaEpics.h
+++ luaApp/src/luaEpics.h
@@ -16,4 +16,7 @@ epicsShareFunc int luaLoadScript(lua_State* state, const char* script_file);
 epicsShareFunc int luaLoadString(lua_State* state, const char* lua_code);
 epicsShareFunc int  luaLoadParams(lua_State* state, const char* param_list);
 epicsShareFunc void luaLoadMacros(lua_State* state, const char* macro_list);
+
+epicsShareFunc void luaRegisterLibrary(const char* library_name, lua_CFunction load_func);
+epicsShareFunc void luaLoadRegisteredLibraries(lua_State* state);
 #endif
