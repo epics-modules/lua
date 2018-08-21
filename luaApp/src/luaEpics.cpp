83852183c33ef218f2136a8c0d27d70f5e457cee
klang <klang@aps.anl.gov>
2018-08-21 15:35:22 -0500

    Added utility to easily bind functions

--- luaApp/src/luaEpics.cpp
+++ luaApp/src/luaEpics.cpp
@@ -193,3 +193,161 @@ epicsShareFunc void luaLoadRegisteredLibraries(lua_State* state)
 		luaL_requiref( state, temp.first, temp.second, 1 );
 	}
 }
+
+
+static int l_call(lua_State* state)
+{	
+	lua_getfield(state, 1, "function_definition");
+	iocshFuncDef const* func_def = (iocshFuncDef const*) lua_touserdata(state, lua_gettop(state));
+	lua_pop(state, 1);
+	
+	lua_getfield(state, 1, "function_pointer");
+	iocshCallFunc to_call = (iocshCallFunc) lua_touserdata(state, lua_gettop(state));
+	lua_pop(state, 1);
+	
+	int given_args = lua_gettop(state) - 1;
+	int needed_args = func_def->nargs;
+		
+	char** argv = (char**) malloc(given_args * sizeof(char*));
+		
+	iocshArgBuf *argBuf = (iocshArgBuf*) malloc(sizeof(iocshArgBuf));
+	int argBufCapacity = 0;
+	
+	for(int index = 0; ; )
+	{
+		if (index >= needed_args)    { break; }
+		if (index >= argBufCapacity) 
+		{
+			void *np;
+			
+			argBufCapacity += 20;
+			np = realloc (argBuf, argBufCapacity * sizeof *argBuf);
+			
+			if (np == NULL) 
+			{
+				printf("Out of memory!\n");
+				argBufCapacity -= 20;
+				break;
+			}
+			
+			argBuf = (iocshArgBuf *)np;
+		}
+		
+		int needed_type = func_def->arg[index]->type;
+		
+		if (needed_type == iocshArgArgv)
+		{
+			argBuf[index].aval.ac = given_args - index + 1;
+			
+			argv[0] = (char*) func_def->name;
+			
+			for (int i = index; i < needed_args ; i += 1)
+			{
+				const char* val = (i < given_args) ? lua_tostring(state, i + 2) : NULL;
+				
+				argv[i - index + 1] = (char*) val;
+			}
+			
+			argBuf[index].aval.av = argv;
+			index = needed_args;
+		}
+		else if(needed_type == iocshArgInt)
+		{
+			int val = (int) (index < given_args) ? lua_tonumber(state, index + 2) : 0;
+		
+			argBuf[index].ival = val;
+		}
+		else if (needed_type == iocshArgDouble)
+		{
+			double val = (index < given_args) ? lua_tonumber(state, index + 2) : 0.0;
+		
+			argBuf[index].dval = val;
+		}
+		else if (needed_type == iocshArgString || needed_type == iocshArgPersistentString)
+		{
+			const char* val = (index < given_args) ? lua_tostring(state, index + 2) : NULL;
+		
+			argBuf[index].sval = (char *) val;
+		}
+		else if (needed_type == iocshArgPdbbase)
+		{
+			void* val = (index < given_args) ? lua_touserdata(state, index + 2) : NULL;
+			
+			argBuf[index].vval = val;
+		}
+		
+		index += 1;
+	}
+			
+	(*to_call)(argBuf);
+	free(argv);
+	free(argBuf);
+	
+	return 0;
+}
+
+
+static int l_index(lua_State* state)
+{
+	static const luaL_Reg func_meta[] = {
+		{"__call", l_call},
+		{NULL, NULL}
+	};
+	
+	
+	const char* temp = lua_tostring(state, 2);
+	std::string func_name(temp);
+	
+	lua_getfield(state, 1, "iocshCmdDef");
+	iocshCmdDef* cmds = (iocshCmdDef*) lua_touserdata(state, lua_gettop(state));
+	lua_pop(state, 1);
+	
+	while (cmds->pFuncDef != NULL && cmds->func != NULL)
+	{
+		if (func_name == cmds->pFuncDef->name)
+		{
+			luaL_newmetatable(state, "func_meta");
+			luaL_setfuncs(state, func_meta, 0);
+			lua_pop(state, 1);
+			
+			lua_newtable(state);
+			luaL_setmetatable(state, "func_meta");
+			
+			lua_pushlightuserdata(state, (void*) cmds->pFuncDef);
+			lua_setfield(state, -2, "function_definition");
+			
+			lua_pushlightuserdata(state, (void*) cmds->func);
+			lua_setfield(state, -2, "function_pointer");
+			
+			return 1;
+		}
+		
+		cmds++;
+	}
+	
+	lua_pushnil(state);
+	return 1;
+}
+
+
+epicsShareFunc void luaBindFunctions(lua_State* state, const iocshCmdDef* cmds)
+{
+	static const luaL_Reg epics_meta[] = {
+		{"__index", l_index},
+		{NULL, NULL}  /* sentinel */
+	};
+	
+	static const luaL_Reg epics_funcs[] = {
+		{NULL, NULL}
+	};
+	
+	luaL_newmetatable(state, "epics_meta");
+	luaL_setfuncs(state, epics_meta, 0);
+	lua_pop(state, 1);
+	
+	luaL_newlib(state, epics_funcs);
+	luaL_setmetatable(state, "epics_meta");
+	
+	lua_pushlightuserdata(state, (void*) cmds);
+	lua_setfield(state, -2, "iocshCmdDef");
+}
--- luaApp/src/luaEpics.h
+++ luaApp/src/luaEpics.h
@@ -2,24 +2,24 @@
 #define INC_LUAEPICS_H
 
 #include <shareLib.h>
+#include <iocsh.h>
 
 #ifdef __cplusplus
-extern "C"
-{
-#endif
-	
-	#include "lua.h"
-	#include "lualib.h"
-	#include "lauxlib.h"
-	
-	
-#ifdef __cplusplus
-}
 
 #include <string>
 
 epicsShareFunc std::string luaLocateFile(std::string filename);
+
+
+
+extern "C"
+{
+	
 #endif
+	
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
 
 epicsShareFunc int luaLoadScript(lua_State* state, const char* script_file);
 epicsShareFunc int luaLoadString(lua_State* state, const char* lua_code);
@@ -28,4 +28,10 @@ epicsShareFunc void luaLoadMacros(lua_State* state, const char* macro_list);
 
 epicsShareFunc void luaRegisterLibrary(const char* library_name, lua_CFunction load_func);
 epicsShareFunc void luaLoadRegisteredLibraries(lua_State* state);
+epicsShareFunc void luaBindFunctions(lua_State* state, const iocshCmdDef*);
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
