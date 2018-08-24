6089f553979bbd2e1d8a172527bf3c0825aa9c3d
klang <klang@aps.anl.gov>
2018-08-24 10:22:31 -0500

    iocsh needs to be available without loading dbd

--- luaApp/src/luaEpics.cpp
+++ luaApp/src/luaEpics.cpp
@@ -11,6 +11,8 @@
 #endif
 
 #include <macLib.h>
+#include <iocsh.h>
+#include <epicsExport.h>
 
 #define epicsExportSharedSymbols
 #include "luaEpics.h"
@@ -212,6 +214,150 @@ epicsShareFunc void luaLoadRegistered(lua_State* state)
 
 
 #ifdef LUA_COMPAT_IOCSH
+static int l_ioccall(lua_State* state)
+{
+	lua_getfield(state, 1, "function_name");
+
+	const char* func_name = lua_tostring(state, lua_gettop(state));
+	lua_pop(state, 1);
+
+	const iocshCmdDef* found = iocshFindCommand(func_name);
+
+	if (found)
+	{
+		int given_args = lua_gettop(state) - 1;
+		int needed_args = found->pFuncDef->nargs;
+
+		char** argv = (char**) malloc(given_args * sizeof(char*));
+
+		iocshArgBuf *argBuf = (iocshArgBuf*) malloc(sizeof(iocshArgBuf));
+		int argBufCapacity = 0;
+
+		for(int index = 0; ; )
+		{
+			if (index >= needed_args)    { break; }
+			if (index >= argBufCapacity)
+			{
+				void *np;
+
+				argBufCapacity += 20;
+				np = realloc (argBuf, argBufCapacity * sizeof *argBuf);
+
+				if (np == NULL)
+				{
+					printf("Out of memory!\n");
+					argBufCapacity -= 20;
+					break;
+				}
+
+				argBuf = (iocshArgBuf *)np;
+			}
+
+			int needed_type = found->pFuncDef->arg[index]->type;
+
+			if (needed_type == iocshArgArgv)
+			{
+				argBuf[index].aval.ac = given_args - index + 1;
+
+				argv[0] = (char*) func_name;
+
+				for (int i = index; i < needed_args ; i += 1)
+				{
+					const char* val = (i < given_args) ? lua_tostring(state, i + 2) : NULL;
+
+					argv[i - index + 1] = (char*) val;
+				}
+
+				argBuf[index].aval.av = argv;
+				index = needed_args;
+			}
+			else if(needed_type == iocshArgInt)
+			{
+				int val = (int) (index < given_args) ? lua_tonumber(state, index + 2) : 0;
+
+				argBuf[index].ival = val;
+			}
+			else if (needed_type == iocshArgDouble)
+			{
+				double val = (index < given_args) ? lua_tonumber(state, index + 2) : 0.0;
+
+				argBuf[index].dval = val;
+			}
+			else if (needed_type == iocshArgString || needed_type == iocshArgPersistentString)
+			{
+				const char* val = (index < given_args) ? lua_tostring(state, index + 2) : NULL;
+
+				argBuf[index].sval = (char *) val;
+			}
+			else if (needed_type == iocshArgPdbbase)
+			{
+				void* val = (index < given_args) ? lua_touserdata(state, index + 2) : NULL;
+
+				argBuf[index].vval = val;
+			}
+
+			index += 1;
+		}
+
+		(*found->func)(argBuf);
+		free(argv);
+		free(argBuf);
+	}
+	else
+	{
+		printf("Command %s not found.\n", func_name);
+	}
+
+	return 0;
+}
+
+static int l_iocindex(lua_State* state)
+{
+	static const luaL_Reg func_meta[] = {
+		{"__call", l_ioccall},
+		{NULL, NULL}
+	};
+	
+	const char* func_name = lua_tostring(state, 2);
+
+	luaL_newmetatable(state, "func_meta");
+	luaL_setfuncs(state, func_meta, 0);
+	lua_pop(state, 1);
+
+	lua_newtable(state);
+	luaL_setmetatable(state, "func_meta");
+
+	lua_pushstring(state, func_name);
+	lua_setfield(state, -2, "function_name");
+
+	return 1;
+}
+
+
+int luaopen_iocsh (lua_State* state)
+{
+	static const luaL_Reg iocsh_meta[] = {
+		{"__index", l_iocindex},
+		{NULL, NULL}  /* sentinel */
+	};
+	
+	static const luaL_Reg iocsh_funcs[] = {
+		{NULL, NULL}
+	};
+	
+	luaL_newmetatable(state, "iocsh_meta");
+	luaL_setfuncs(state, iocsh_meta, 0);
+	lua_pop(state, 1);
+
+	luaL_newlib(state, iocsh_funcs);
+	luaL_setmetatable(state, "iocsh_meta");
+
+	return 1;
+}
+
+
+
+
 static int l_call(lua_State* state)
 {	
 	lua_getfield(state, 1, "function_definition");
@@ -376,6 +522,10 @@ epicsShareFunc lua_State* luaCreateState()
 	lua_State* output = luaL_newstate();
 	luaL_openlibs(output);
 	luaLoadRegistered(output);
+	
+	#ifdef LUA_COMPAT_IOCSH
+	luaL_requiref(output, "iocsh", luaopen_iocsh, 1);
+	#endif
 	
 	return output;
 }
