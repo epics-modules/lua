ccfe803ad746322336ad5b34a44bb2094a5c35d7
Keenan Lang <klang@aps.anl.gov>
2021-02-04 00:58:20 -0600

    Copy syncwrite changes to asyncwrite

--- luaApp/src/rec/luaSoft.c
+++ luaApp/src/rec/luaSoft.c
@@ -4,6 +4,7 @@
 
 #include <callback.h>
 #include <dbCa.h>
+#include <errlog.h>
 
 #include "alarm.h"
 #include "dbDefs.h"
@@ -47,10 +48,127 @@ static long asyncWrite(luascriptRecord* record, double* val, char* sval, struct 
 	short   field_type = 0;
 
 	field_type = dbCaGetLinkDBFtype(out);
+	dbCaGetNelements(out, &n_elements);
 
 	if (devLuaSoftDebug)
 		{ printf("write_Script: field_type=%d\n", field_type); }
 
+	int bytes_per_elem = 1;
+	
+	if (record->atyp == luascriptAVALType_Integer)   { bytes_per_elem = sizeof(int); }
+	if (record->atyp == luascriptAVALType_Double)    { bytes_per_elem = sizeof(double); }
+	if (record->atyp == luascriptAVALType_Char)      { bytes_per_elem = sizeof(char); }
+	
+	
+	int array_len = (int) record->asiz / bytes_per_elem;
+
+	
+	if (n_elements > 1)
+	{
+		int index;
+		
+		if (array_len > n_elements) 
+		{
+			errlogPrintf("%s: Array too large to write to output\n", record->name);
+			return 0;
+		}
+
+		switch (field_type)
+		{
+			case DBF_CHAR:
+			case DBF_UCHAR:
+			{				
+				if (record->atyp == luascriptAVALType_Char)
+				{
+					return dbCaPutLinkCallback(out, DBF_CHAR, record->aval, array_len, (dbCaCallback) dbCaCallbackProcess, out);
+
+				}
+				else if (record->atyp == luascriptAVALType_Integer)
+				{
+					#if defined(_MSC_VER)
+						char buffer[1024];
+					#else
+						char buffer[array_len];
+					#endif
+
+					for (index = 0; index < n_elements && index < array_len; index += 1)
+					{
+						buffer[index] = (char) ((int*) record->aval)[index];
+					}
+
+					return dbCaPutLinkCallback(out, DBF_CHAR, buffer, array_len, (dbCaCallback) dbCaCallbackProcess, out);
+				}
+
+				return 0;
+			}
+
+			case DBF_DOUBLE:
+			{
+				if (record->atyp == luascriptAVALType_Double)
+				{
+					return dbCaPutLinkCallback(out, DBF_DOUBLE, record->aval, array_len, (dbCaCallback) dbCaCallbackProcess, out);
+				}
+				else if (record->atyp == luascriptAVALType_Integer)
+				{
+					#if defined(_MSC_VER)
+						double buffer[1024];
+					#else
+						double buffer[array_len];
+					#endif
+
+					for (index = 0; index < n_elements && index < array_len; index += 1)
+					{
+						buffer[index] = ((int*) record->aval)[index];
+					}
+
+					return dbCaPutLinkCallback(out, DBF_DOUBLE, buffer, array_len, (dbCaCallback) dbCaCallbackProcess, out);
+				}
+
+				return 0;
+			}
+
+			case DBF_FLOAT:
+			{
+				#if defined(_MSC_VER)
+					float buffer[1024];
+				#else
+					float buffer[array_len];
+				#endif
+
+				if (record->atyp == luascriptAVALType_Double)
+				{
+					for(index = 0; index < n_elements && index < array_len; index += 1)
+					{
+						buffer[index] = (float) ((double*) record->aval)[index];
+					}
+
+					return dbCaPutLinkCallback(out, DBF_FLOAT, buffer, array_len, (dbCaCallback) dbCaCallbackProcess, out);
+				}
+				else if(record->atyp == luascriptAVALType_Integer)
+				{
+					for(index = 0; index < n_elements && index < array_len; index += 1)
+					{
+						buffer[index] = (float) ((int*) record->aval)[index];
+					}
+
+					return dbCaPutLinkCallback(out, DBF_FLOAT, buffer, array_len, (dbCaCallback) dbCaCallbackProcess, out);
+				}
+
+				return 0;
+			}
+
+			default:
+			{
+				if (record->atyp == luascriptAVALType_Integer)
+				{
+					dbCaPutLinkCallback(out, DBF_LONG, record->aval, array_len, (dbCaCallback) dbCaCallbackProcess, out);
+				}
+
+				return 0;
+			}
+		}
+	}
+	
 	switch (field_type)
 	{
 		case DBF_STRING:
@@ -118,7 +236,7 @@ static long syncWrite(luascriptRecord* record, double* val, char* sval, struct l
 	}
 	else if (out->type == DB_LINK)
 	{
-		if (!dbNameToAddr(out->value.pv_link.pvname, pAddr))
+		 if (!dbNameToAddr(out->value.pv_link.pvname, pAddr))
 		{
 			field_type = pAddr->field_type;
 			n_elements = pAddr->no_elements;
