36c0417c346bb7a51ad48114e2a8409b60131da3
keenanlang <klang@aps.anl.gov>
2022-07-22 23:09:43 -0500

    Implemented static db access API (mostly)

--- docs/database-library.rst
+++ docs/database-library.rst
@@ -2,6 +2,77 @@
 Database Library Documentation
 ==============================
 
+**db.entry** ()
+
+::
+
+   Creates a DBENTRY pointer which can be used with all implemented static database access functions.
+   dbFreeEntry will be called on the pointer automatically when the entry is garbage collected.
+
+
+Static Database Access
+----------------------
+
+The following list of static database access functions are implemented, largely unchanged from their
+C API. Naming conventions have been changed to drop the initial "db" prefix and for the next character
+to be lowercase; so, for example, dbGetFieldName would become the module function getFieldName. 
+
+Functions which would return a status code, like dbFindRecord, will instead return a boolean representing
+success or failure (true or false respectively). Where a DBENTRY pointer is required as a parameter, instead
+instances of the dbentry class will be used, created with the aforementioned db.entry function.
+
+* getNRecordTypes
+* findRecordType
+* firstRecordType
+* nextRecordType
+* getRecordTypeName
+* getNFields
+* firstField
+* nextField
+* getFieldDbfType
+* getFieldName
+* getDefault
+* getPrompt
+* getPromptGroup
+* putRecordAttribute
+* getRecordAttribute
+* getNAliases
+* getNRecords
+* findRecord
+* firstRecord
+* nextRecord
+* getRecordName
+* isAlias
+* createRecord
+* createAlias
+* deleteRecord
+* deleteAliases
+* copyRecord
+* findField
+* foundField
+* getString
+* putString
+* isDefaultValue
+* getNMenuChoices
+* getMenuIndex
+* putMenuIndex
+* getMenuStringFromIndex
+* getMenuIndexFromString
+* getNLinks
+* getLinkField
+* firstInfo
+* nextInfo
+* findInfo
+* getInfoName
+* getInfoString
+* putInfoString
+* putInfo
+* deleteInfo
+* getInfo
+
+
+
+
 **db.record** (recordtype, recordname)
 
 ::
@@ -22,7 +93,7 @@ Database Library Documentation
    With lua syntactical sugar, the generation and calling of the table can be combined.
    An example of this would be:
 
-   db.record("ai", "x:y:z") {
+   db.record("ai
       DTYP = "asynInt32",
       INP = "@asyn(A_PORT,0,1)PARAM_NAME"
    }
--- luaApp/src/libs/ldatabaselib.cpp
+++ luaApp/src/libs/ldatabaselib.cpp
@@ -3,6 +3,154 @@
 #include <epicsExport.h>
 #include "luaEpics.h"
 
+class _entry
+{
+	private:
+		_entry()    { this->entry = dbAllocEntry(*iocshPpdbbase); }
+		~_entry()   { dbFreeEntry(this->entry); }
+		
+	public:
+		DBENTRY* entry = NULL;
+		
+		static _entry* create(lua_State* state)
+		{			
+			if (! iocshPpdbbase)    { luaL_error(state, "No database definition found.\n"); }
+			
+			return new _entry();
+		}
+		
+		static void destroy(_entry* instance)    { delete instance; }
+};
+
+_entry* genEntry(lua_State* state)
+{
+	return _entry::create(state);
+}
+
+
+std::string fieldName(_entry* in)
+{
+	char* output = dbGetFieldName(in->entry);
+	
+	if (output)    { return std::string(output); }
+	else           { return std::string(""); }
+}
+
+std::string defaultVal(_entry* in)
+{
+	char* output = dbGetDefault(in->entry);
+	
+	if (output)    { return std::string(output); }
+	else           { return std::string(""); }
+}
+
+std::string recordTypeName(_entry* in)
+{
+	char* output = dbGetRecordTypeName(in->entry);
+	
+	if (output)    { return std::string(output); }
+	else           { return std::string(""); }
+}
+
+std::string recordName(_entry* in)
+{
+	char* output = dbGetRecordName(in->entry);
+	
+	if (output)    { return std::string(output); }
+	else           { return std::string(""); }
+}
+
+std::string getString(_entry* in)
+{
+	char* output = dbGetString(in->entry);
+	
+	if (output)    { return std::string(output); }
+	else           { return std::string(""); }
+}
+
+std::string getPrompt(_entry* in)
+{
+	char* output = dbGetPrompt(in->entry);
+	
+	if (output)    { return std::string(output); }
+	else           { return std::string(""); }
+}
+
+std::string getMenuFromIndex(_entry* in, int index)
+{
+	char* output = dbGetMenuStringFromIndex(in->entry, index);
+	
+	if (output)    { return std::string(output); }
+	else           { return std::string(""); }
+}
+
+std::string getInfoName(_entry* in)
+{
+	const char* output = dbGetInfoName(in->entry);
+	
+	if (output)    { return std::string(output); }
+	else           { return std::string(""); }
+}
+
+std::string getInfoString(_entry* in)
+{
+	const char* output = dbGetInfoString(in->entry);
+	
+	if (output)    { return std::string(output); }
+	else           { return std::string(""); }
+}
+
+std::string getInfo(_entry* in, const char* name)
+{
+	const char* output = dbGetInfo(in->entry, name);
+	
+	if (output)    { return std::string(output); }
+	else           { return std::string(""); }
+}
+
+bool putAttr(_entry* in, const char* key, const char* val)    { return (dbPutRecordAttribute(in->entry, key, val) == 0); }
+bool putInfo(_entry* in, const char* key, const char* val)    { return (dbPutInfo(in->entry, key, val) == 0); }
+
+bool getAttr(_entry* in, const char* key)              { return (dbGetRecordAttribute(in->entry, key) == 0); }
+bool findRecord(_entry* in, const char* name)          { return (dbFindRecord(in->entry, name) == 0); }
+bool createRecord(_entry* in, const char* name)        { return (dbCreateRecord(in->entry, name) == 0); }
+bool createAlias(_entry* in, const char* name)         { return (dbCreateAlias(in->entry, name) == 0); }
+bool deleteAliases(_entry* in)                         { return (dbDeleteAliases(in->entry) == 0); }
+bool copyRecord(_entry* in, const char* name, int ow)  { return (dbCopyRecord(in->entry, name, ow) == 0); }
+bool deleteRecord(_entry* in)                          { return (dbDeleteRecord(in->entry) == 0); }
+bool findRecordType(_entry* in, const char* name)      { return (dbFindRecordType(in->entry, name) == 0); }
+bool findField(_entry* in, const char* name)           { return dbFindField(in->entry, name); }
+bool putString(_entry* in, const char* value)          { return (dbPutString(in->entry, value) == 0); }
+bool putIndex(_entry* in, int index)                   { return (dbPutMenuIndex(in->entry, index) == 0); }
+bool linkField(_entry* in, int index)                  { return (dbGetLinkField(in->entry, index) == 0); }
+bool findInfo(_entry* in, const char* name)            { return (dbFindInfo(in->entry, name) == 0); }
+bool putInfoString(_entry* in, const char* val)        { return (dbPutInfoString(in->entry, val) == 0); }
+
+bool firstRecord(_entry* in)      { return (dbFirstRecord(in->entry) == 0); }
+bool nextRecord(_entry* in)       { return (dbNextRecord(in->entry) == 0); }
+bool isAlias(_entry* in)          { return dbIsAlias(in->entry); }
+bool firstField(_entry* in)       { return (dbFirstField(in->entry, 0) == 0); }
+bool nextField(_entry* in)        { return (dbNextField(in->entry, 0) == 0); }
+bool firstRecordType(_entry* in)  { return (dbFirstRecordType(in->entry) == 0); }
+bool nextRecordType(_entry* in)   { return (dbNextRecordType(in->entry) == 0); }
+bool foundField(_entry* in)       { return dbFoundField(in->entry); }
+bool isDefault(_entry* in)        { return dbIsDefaultValue(in->entry); }
+bool firstInfo(_entry* in)        { return (dbFirstInfo(in->entry) == 0); }
+bool nextInfo(_entry* in)         { return (dbNextInfo(in->entry) == 0); }
+bool deleteInfo(_entry* in)       { return (dbDeleteInfo(in->entry) == 0); }
+
+int numFields(_entry* in)       { return dbGetNFields(in->entry, 0); }
+int numRecords(_entry* in)      { return dbGetNRecords(in->entry); }
+int numAliases(_entry* in)      { return dbGetNAliases(in->entry); }
+int numRecordTypes(_entry* in)  { return dbGetNRecordTypes(in->entry); }
+int fieldType(_entry* in)       { return dbGetFieldDbfType(in->entry); }
+int promptGroup(_entry* in)     { return dbGetPromptGroup(in->entry); }
+int numChoices(_entry* in)      { return dbGetNMenuChoices(in->entry); }
+int getIndex(_entry* in)        { return dbGetMenuIndex(in->entry); }
+int numLinks(_entry* in)        { return dbGetNLinks(in->entry); }
+
+int getIndexFromMenu(_entry* in, const char* name)    { return dbGetMenuIndexFromString(in->entry, name); }
+
 int l_setfield(lua_State* state)
 {
 	DBENTRY* entry = dbAllocEntry(*iocshPpdbbase);
@@ -85,6 +233,76 @@ int l_record(lua_State* state)
 
 int luaopen_database (lua_State *L)
 {
+	LuaClass<_entry> lua_ew(L, "dbentry");
+	lua_ew.ctor<void, lua_State*>("new",&_entry::create, &_entry::destroy);
+	
+	LuaModule dbmod (L, "db");
+	dbmod.fun("entry", genEntry);
+	
+	dbmod.fun("getNRecordTypes", numRecordTypes);
+	dbmod.fun("findRecordType", findRecordType);
+	dbmod.fun("firstRecordType", firstRecordType);
+	dbmod.fun("nextRecordType", nextRecordType);
+	dbmod.fun("getRecordTypeName", recordTypeName);
+	
+	dbmod.fun("getNFields", numFields);
+	dbmod.fun("firstField", firstField);
+	dbmod.fun("nextField", nextField);
+	dbmod.fun("getFieldDbfType", fieldType);
+	dbmod.fun("getFieldName", fieldName);
+
+	dbmod.fun("getDefault", defaultVal);
+	
+	dbmod.fun("getPrompt", getPrompt);
+	dbmod.fun("getPromptGroup", promptGroup);
+	
+	dbmod.fun("putRecordAttribute", putAttr);
+	dbmod.fun("getRecordAttribute", getAttr);
+	
+	dbmod.fun("getNAliases", numAliases);
+	
+	dbmod.fun("getNRecords", numRecords);
+	dbmod.fun("findRecord", findRecord);
+	dbmod.fun("firstRecord", firstRecord);
+	dbmod.fun("nextRecord", nextRecord);
+	dbmod.fun("getRecordName", recordName);
+	
+	dbmod.fun("isAlias", isAlias);
+	
+	dbmod.fun("createRecord", createRecord);
+	dbmod.fun("createAlias", createAlias);
+	dbmod.fun("deleteRecord", deleteRecord);
+	dbmod.fun("deleteAliases", deleteAliases);
+	
+	dbmod.fun("copyRecord", copyRecord);
+	
+	dbmod.fun("findField", findField);
+	dbmod.fun("foundField", foundField);
+	
+	dbmod.fun("getString", getString);
+	dbmod.fun("putString", putString);
+	dbmod.fun("isDefaultValue", isDefault);
+	
+	dbmod.fun("getNMenuChoices", numChoices);
+	dbmod.fun("getMenuIndex", getIndex);
+	dbmod.fun("putMenuIndex", putIndex);
+	dbmod.fun("getMenuStringFromIndex", getMenuFromIndex);
+	dbmod.fun("getMenuIndexFromString", getIndexFromMenu);
+	
+	dbmod.fun("getNLinks", numLinks);
+	dbmod.fun("getLinkField", linkField);
+	
+	dbmod.fun("firstInfo", firstInfo);
+	dbmod.fun("nextInfo", nextInfo);
+	dbmod.fun("findInfo", findInfo);
+	dbmod.fun("getInfoName", getInfoName);
+	dbmod.fun("getInfoString", getInfoString);
+	dbmod.fun("putInfoString", putInfoString);
+	dbmod.fun("putInfo", putInfo);
+	dbmod.fun("deleteInfo", deleteInfo);
+	dbmod.fun("getInfo", getInfo);
+	
+	
 	static const luaL_Reg rec_meta[] = {
 		{"__call", l_setfield},
 		{NULL, NULL}
@@ -93,13 +311,10 @@ int luaopen_database (lua_State *L)
 	luaL_newmetatable(L, "rec_meta");
 	luaL_setfuncs(L, rec_meta, 0);
 	lua_pop(L, 1);
-
-	static const luaL_Reg mylib[] = {
-		{"record", l_record},
-		{NULL, NULL}  /* sentinel */
-	};
-
-	luaL_newlib(L, mylib);
+	
+	dbmod.fun("record", l_record);
+	
+	lua_getglobal(L, "db");
 	return 1;
 }
 
