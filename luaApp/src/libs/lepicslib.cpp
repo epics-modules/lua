7e0a3fbe740db526aefc8f499736fed133b24f5b
keenanlang <klang@aps.anl.gov>
2026-05-18 13:07:11 -0500

    return error messages and cache ca context

--- luaApp/src/libs/lepicslib.cpp
+++ luaApp/src/libs/lepicslib.cpp
@@ -1,42 +1,76 @@
 #include <cadef.h>
 #include <string>
 #include <stdio.h>
-#include <epicsThread.h>
 #include <epicsExport.h>
 #include "lepicslib.h"
 
+
+/*
+ * CA context management -- cached per Lua state.
+ *
+ * A sentinel userdata is stored in the Lua registry. Its __gc
+ * destroys the CA context when the Lua state is closed. The
+ * context is created once on first use and reused for all
+ * subsequent epics.get/put/pv calls in that state.
+ */
+#define LEPICS_CA_CONTEXT_KEY "LEPICS_CA_CONTEXT"
+
+static int l_ca_context_gc(lua_State* L)
+{
+	if (ca_current_context())    { ca_context_destroy(); }
+	return 0;
+}
+
+static void ensure_ca_context(lua_State* L)
+{
+	if (ca_current_context())    { return; }
+
+	ca_context_create(ca_enable_preemptive_callback);
+
+	/* Create a sentinel userdata with __gc to destroy the context */
+	lua_newuserdata(L, 1);
+
+	if (luaL_newmetatable(L, "lepics_ca_gc"))
+	{
+		lua_pushcfunction(L, l_ca_context_gc);
+		lua_setfield(L, -2, "__gc");
+	}
+	lua_setmetatable(L, -2);
+
+	lua_setfield(L, LUA_REGISTRYINDEX, LEPICS_CA_CONTEXT_KEY);
+}
+
+
 static int epics_get(lua_State* state, const char* pv_name, double timeout)
 {
-	if (pv_name == NULL)    { return 0; }
-
-	int created_context = 0;
-
-	if (!ca_current_context())
+	if (pv_name == NULL)
 	{
-		int status = ca_context_create(ca_enable_preemptive_callback);
-		SEVCHK(status, NULL);
-		created_context = 1;
+		lua_pushnil(state);
+		lua_pushstring(state, "PV name is nil");
+		return 2;
 	}
+
+	ensure_ca_context(state);
 
 	chid id;
 
 	int status = ca_create_channel(pv_name, NULL, NULL, 0, &id);
-	SEVCHK(status, NULL);
 
 	if (status != ECA_NORMAL)
 	{
-		if (created_context) ca_context_destroy();
-		return 0;
+		lua_pushnil(state);
+		lua_pushfstring(state, "Failed to create channel for '%s'", pv_name);
+		return 2;
 	}
 
 	status = ca_pend_io(timeout);
-	SEVCHK(status, NULL);
 
 	if (status != ECA_NORMAL)
 	{
 		ca_clear_channel(id);
-		if (created_context) ca_context_destroy();
-		return 0;
+		lua_pushnil(state);
+		lua_pushfstring(state, "Timeout connecting to '%s'", pv_name);
+		return 2;
 	}
 
 	int result = 0;
@@ -51,7 +85,6 @@ static int epics_get(lua_State* state, const char* pv_name, double timeout)
 		{
 			struct dbr_time_string val;
 			status = ca_get(DBR_TIME_STRING, id, &val);
-			SEVCHK(status, NULL);
 			status = ca_pend_io(timeout);
 			if (status == ECA_NORMAL) { lua_pushstring(state, val.value); result = 1; }
 			break;
@@ -61,7 +94,6 @@ static int epics_get(lua_State* state, const char* pv_name, double timeout)
 		{
 			struct dbr_time_enum val;
 			status = ca_get(DBR_TIME_ENUM, id, &val);
-			SEVCHK(status, NULL);
 			status = ca_pend_io(timeout);
 			if (status == ECA_NORMAL) { lua_pushnumber(state, val.value); result = 1; }
 			break;
@@ -71,7 +103,6 @@ static int epics_get(lua_State* state, const char* pv_name, double timeout)
 		{
 			struct dbr_time_char val;
 			status = ca_get(DBR_TIME_CHAR, id, &val);
-			SEVCHK(status, NULL);
 			status = ca_pend_io(timeout);
 			if (status == ECA_NORMAL) { lua_pushnumber(state, val.value); result = 1; }
 			break;
@@ -81,7 +112,6 @@ static int epics_get(lua_State* state, const char* pv_name, double timeout)
 		{
 			struct dbr_time_short val;
 			status = ca_get(DBR_TIME_SHORT, id, &val);
-			SEVCHK(status, NULL);
 			status = ca_pend_io(timeout);
 			if (status == ECA_NORMAL) { lua_pushnumber(state, val.value); result = 1; }
 			break;
@@ -91,7 +121,6 @@ static int epics_get(lua_State* state, const char* pv_name, double timeout)
 		{
 			struct dbr_time_long val;
 			status = ca_get(DBR_TIME_LONG, id, &val);
-			SEVCHK(status, NULL);
 			status = ca_pend_io(timeout);
 			if (status == ECA_NORMAL) { lua_pushnumber(state, val.value); result = 1; }
 			break;
@@ -101,7 +130,6 @@ static int epics_get(lua_State* state, const char* pv_name, double timeout)
 		{
 			struct dbr_time_float val;
 			status = ca_get(DBR_TIME_FLOAT, id, &val);
-			SEVCHK(status, NULL);
 			status = ca_pend_io(timeout);
 			if (status == ECA_NORMAL) { lua_pushnumber(state, val.value); result = 1; }
 			break;
@@ -111,7 +139,6 @@ static int epics_get(lua_State* state, const char* pv_name, double timeout)
 		{
 			struct dbr_time_double val;
 			status = ca_get(DBR_TIME_DOUBLE, id, &val);
-			SEVCHK(status, NULL);
 			status = ca_pend_io(timeout);
 			if (status == ECA_NORMAL) { lua_pushnumber(state, val.value); result = 1; }
 			break;
@@ -120,43 +147,46 @@ static int epics_get(lua_State* state, const char* pv_name, double timeout)
 
 	ca_clear_channel(id);
 
-	if (created_context) ca_context_destroy();
+	if (result == 0)
+	{
+		lua_pushnil(state);
+		lua_pushfstring(state, "Failed to read '%s'", pv_name);
+		return 2;
+	}
 
 	return result;
 }
 
 static int epics_put(lua_State* state, const char* pv_name, int offset, double timeout)
 {
-	if (pv_name == NULL)    { return 0; }
-
-	int created_context = 0;
-
-	if (!ca_current_context())
+	if (pv_name == NULL)
 	{
-		int status = ca_context_create(ca_enable_preemptive_callback);
-		SEVCHK(status, NULL);
-		created_context = 1;
+		lua_pushnil(state);
+		lua_pushstring(state, "PV name is nil");
+		return 2;
 	}
+
+	ensure_ca_context(state);
 
 	chid id;
 
 	int status = ca_create_channel(pv_name, NULL, NULL, 0, &id);
-	SEVCHK(status, NULL);
 
 	if (status != ECA_NORMAL)
 	{
-		if (created_context) ca_context_destroy();
-		return 0;
+		lua_pushnil(state);
+		lua_pushfstring(state, "Failed to create channel for '%s'", pv_name);
+		return 2;
 	}
 
 	status = ca_pend_io(timeout);
-	SEVCHK(status, NULL);
 
 	if (status != ECA_NORMAL)
 	{
 		ca_clear_channel(id);
-		if (created_context) ca_context_destroy();
-		return 0;
+		lua_pushnil(state);
+		lua_pushfstring(state, "Timeout connecting to '%s'", pv_name);
+		return 2;
 	}
 
 	switch (lua_type(state, offset))
@@ -182,27 +212,28 @@ static int epics_put(lua_State* state, const char* pv_name, int offset, double t
 			break;
 		}
 
-		case LUA_TNIL:
-			break;
-
-		case LUA_TTABLE:
-		case LUA_TFUNCTION:
-		case LUA_TUSERDATA:
-		case LUA_TTHREAD:
-		case LUA_TLIGHTUSERDATA:
-			/* Unsupported types */
-			break;
+		default:
+		{
+			ca_clear_channel(id);
+			lua_pushnil(state);
+			lua_pushfstring(state, "Unsupported value type for put to '%s'", pv_name);
+			return 2;
+		}
 	}
 
-	SEVCHK(status, NULL);
+	if (status != ECA_NORMAL)
+	{
+		ca_clear_channel(id);
+		lua_pushnil(state);
+		lua_pushfstring(state, "Failed to put value to '%s'", pv_name);
+		return 2;
+	}
 
 	ca_pend_io(timeout);
-
 	ca_clear_channel(id);
 
-	if (created_context) ca_context_destroy();
-
-	return 0;
+	lua_pushboolean(state, 1);
+	return 1;
 }
 
 
@@ -226,17 +257,6 @@ static int l_caput(lua_State* state)
 	if (lua_gettop(state) >= 3)    { timeout = luaL_checknumber(state, 3); }
 
 	return epics_put(state, pv_name, 2, timeout);
-}
-
-static int l_epicssleep(lua_State* state)
-{
-	printf("This function is now deprecated. Use osi.sleep instead\n");
-
-	double seconds = lua_tonumber(state, 1);
-
-	epicsThreadSleep(seconds);
-
-	return 0;
 }
 
 static int l_pvgetval(lua_State* state)
@@ -332,7 +352,6 @@ int luaopen_epics (lua_State *L)
 	static const luaL_Reg mylib[] = {
 		{"get",   l_caget},
 		{"put",   l_caput},
-		{"sleep", l_epicssleep},
 		{"pv",    l_createpv},
 		{NULL, NULL}
 	};
