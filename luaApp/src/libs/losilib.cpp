762eef19d29b5e706d1dfb9565204e5d89fd80a4
keenanlang <klang@aps.anl.gov>
2026-05-31 02:15:20 -0500

    monotonic time, epics timestamp, and string conversion

--- luaApp/src/libs/losilib.cpp
+++ luaApp/src/libs/losilib.cpp
@@ -1,4 +1,6 @@
 #include <epicsThread.h>
+#include <epicsTime.h>
+#include <epicsVersion.h>
 #include <epicsExport.h>
 #include <epicsStdio.h>
 #include "luaEpics.h"
@@ -70,6 +72,90 @@ static int l_osisleep(lua_State* state)
 	return 0;
 }
 
+
+/*
+ * osi.monotonic() -- monotonic time in seconds as a double.
+ * Suitable for measuring intervals. Not affected by system clock
+ * adjustments.
+ */
+static int l_osimonotonic(lua_State* state)
+{
+#if EPICS_VERSION_INT >= VERSION_INT(7, 0, 0, 0)
+	epicsUInt64 ns = epicsMonotonicGet();
+	lua_pushnumber(state, ns * 1e-9);
+#else
+	/* Fallback for base 3.15: use epicsTimeGetCurrent */
+	epicsTimeStamp ts;
+	epicsTimeGetCurrent(&ts);
+	lua_pushnumber(state, ts.secPastEpoch + ts.nsec * 1e-9);
+#endif
+	return 1;
+}
+
+
+/*
+ * osi.time() -- current EPICS time as a double.
+ * Returns seconds past the EPICS epoch (January 1, 1990 00:00:00 UTC)
+ * with sub-second precision.
+ */
+static int l_ositime(lua_State* state)
+{
+	epicsTimeStamp ts;
+	epicsTimeGetCurrent(&ts);
+	lua_pushnumber(state, ts.secPastEpoch + ts.nsec * 1e-9);
+	return 1;
+}
+
+
+/*
+ * osi.timestr([time] [, format]) -- format a timestamp as a string.
+ *
+ * Arguments are disambiguated by type:
+ *   osi.timestr()                  -- current time, default format
+ *   osi.timestr("format")          -- current time, custom format
+ *   osi.timestr(time)              -- specific time, default format
+ *   osi.timestr(time, "format")    -- specific time, custom format
+ *
+ * The default format is "%Y-%m-%d %H:%M:%S.%03f" (millisecond precision).
+ * The %0Nf specifier is an EPICS extension for fractional seconds with
+ * N digits of precision.
+ */
+static int l_ositimestr(lua_State* state)
+{
+	epicsTimeStamp ts;
+	const char* fmt = "%Y-%m-%d %H:%M:%S.%03f";
+
+	if (lua_isnumber(state, 1))
+	{
+		/* First arg is a timestamp */
+		double t = lua_tonumber(state, 1);
+		ts.secPastEpoch = (epicsUInt32) t;
+		ts.nsec = (epicsUInt32) ((t - ts.secPastEpoch) * 1e9);
+
+		if (lua_isstring(state, 2))
+		{
+			fmt = lua_tostring(state, 2);
+		}
+	}
+	else if (lua_isstring(state, 1))
+	{
+		/* First arg is a format string, use current time */
+		fmt = lua_tostring(state, 1);
+		epicsTimeGetCurrent(&ts);
+	}
+	else
+	{
+		/* No args, use current time and default format */
+		epicsTimeGetCurrent(&ts);
+	}
+
+	char buf[64];
+	epicsTimeToStrftime(buf, sizeof(buf), fmt, &ts);
+	lua_pushstring(state, buf);
+	return 1;
+}
+
+
 int luaopen_osi (lua_State *L)
 {
 	lua_newtable(L);
@@ -79,6 +165,9 @@ int luaopen_osi (lua_State *L)
 		{"startRedirectOut", l_setStdout},
 		{"endRedirectOut",   l_resetStdout},
 		{"sleep",            l_osisleep},
+		{"monotonic",        l_osimonotonic},
+		{"time",             l_ositime},
+		{"timestr",          l_ositimestr},
 		{NULL, NULL}
 	};
 
@@ -87,8 +176,11 @@ int luaopen_osi (lua_State *L)
 	/* Documentation for info(osi) */
 	lua_newtable(L);
 	lua_pushstring(L, ".sleep(seconds)"); lua_rawseti(L, -2, 1);
-	lua_pushstring(L, ".startRedirectOut(filename)"); lua_rawseti(L, -2, 2);
-	lua_pushstring(L, ".endRedirectOut()"); lua_rawseti(L, -2, 3);
+	lua_pushstring(L, ".monotonic()                -- monotonic time in seconds"); lua_rawseti(L, -2, 2);
+	lua_pushstring(L, ".time()                     -- EPICS epoch time in seconds"); lua_rawseti(L, -2, 3);
+	lua_pushstring(L, ".timestr([time] [, format]) -- format time as string"); lua_rawseti(L, -2, 4);
+	lua_pushstring(L, ".startRedirectOut(filename)"); lua_rawseti(L, -2, 5);
+	lua_pushstring(L, ".endRedirectOut()"); lua_rawseti(L, -2, 6);
 	lua_setfield(L, -2, "_doc");
 
 	return 1;
